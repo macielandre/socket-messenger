6f2db41e851c56c5ca5e648b42bd99634fd975e7
macielandre <maciel.andre@live.com>
2023-05-07 17:55:13 -0300

    feat: login page style

--- client/App.js
+++ client/App.js
@@ -1,21 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, StyleSheet, TextInput, Text, Button, TouchableOpacity } from 'react-native'
+import Login from './src/components/login'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  submit() {
+    console.warn(this.state)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}> 
+        <Text style={styles.loginText}>
+          YOUR ACCOUNT HERE
+        </Text>
+        <View>
+          <TextInput 
+            placeholder='EMAIL'
+            placeholderTextColor={'gray'}
+            placeholderTextOpacity={0.5}
+            onChangeText={(text) => { 
+              this.setState({email: text})
+            }}
+            style={styles.textInputs}
+          />
+          <TextInput 
+            placeholder='PASSWORD'
+            placeholderTextColor={'gray'}
+            onChangeText={(text) => { 
+              this.setState({password: text})
+            }}
+            style={styles.textInputs}
+          />
+        </View>
+        
+        <TouchableOpacity style={styles.loginButton} onPress={() => {this.submit()}}>
+          <Text style={styles.submitText}>LOGIN</Text>
+        </TouchableOpacity>
+      </View>
+      )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  loginText: {
+    color: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize: 23,
+    marginBottom: 20,
+    fontWeight: 'bold',
   },
-});
+  submitText: {
+    color: '#7B86EC',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 23,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  textInputs: {
+    backgroundColor: '#D9D9D9',
+    fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#CB9D25',
+    borderWidth: 3,
+    width: 330,
+    height: 55,
+    margin: 15,
+    paddingLeft: 16
+  },
+  loginButton: {
+    marginTop: 20,
+    backgroundColor: '#5237F5',
+    fontWeight: 'bold',
+    borderColor: '#1A0790',
+    borderWidth: 3,
+    height: 50,
+    width: 150,
+    padding: 10,
+    justifyContent: 'center',
+    borderRadius: 15,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#EEAC2C',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 50
+  }
+})
